354446026de324f6f899883a6ffd5bb1f00b03f3
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-04-22 10:22:01 -0500

    Solution in Authentication and Authorization

--- ECommerce_Sat/ECommerce_Sat/Program.cs
+++ ECommerce_Sat/ECommerce_Sat/Program.cs
@@ -67,8 +67,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
 app.UseAuthentication(); //Autenticar mi usuario
+app.UseAuthorization();
 
 app.UseStatusCodePagesWithReExecute("/error/{0}");
 
