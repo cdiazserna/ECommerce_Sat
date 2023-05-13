5f2a101917370562dd9632debb8e3eb9c9c7724e
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-13 10:58:24 -0500

    Create GET & POST actions developed

--- ECommerce_Sat/ECommerce_Sat/Controllers/ProductsController.cs
+++ ECommerce_Sat/ECommerce_Sat/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
 ﻿using ECommerce_Sat.DAL;
+using ECommerce_Sat.DAL.Entities;
 using ECommerce_Sat.Helpers;
+using ECommerce_Sat.Models;
+using ECommerce_Sat.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -28,5 +31,77 @@ namespace ECommerce_Sat.Controllers
                 .ThenInclude(pc => pc.Category)
                 .ToListAsync());
         }
+
+        public async Task<IActionResult> Create()
+        {
+            AddProductViewModel addProductViewModel = new()
+            {
+                Categories = await _dropDownListHelper.GetDDLCategoriesAsync(),
+            };
+
+            return View(addProductViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AddProductViewModel addProductViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Guid imageId = Guid.Empty;
+                    if (addProductViewModel.ImageFile != null)
+                        imageId = await _azureBlobHelper.UploadAzureBlobAsync(addProductViewModel.ImageFile, "products");
+
+                    Product product = new()
+                    {
+                        Description = addProductViewModel.Description,
+                        Name = addProductViewModel.Name,
+                        Price = addProductViewModel.Price,
+                        Stock = addProductViewModel.Stock,
+                    };
+
+                    product.ProductCategories = new List<ProductCategory>()
+                    {
+                        new ProductCategory
+                        {
+                            Category = await _context.Categories.FindAsync(addProductViewModel.CategoryId)
+                        }
+                    };
+
+                    if (imageId != Guid.Empty)
+                    {
+                        product.ProductImages = new List<ProductImage>()
+                        {
+                            new ProductImage { ImageId = imageId }
+                        };
+                    }
+
+                    _context.Add(product);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un producto con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            addProductViewModel.Categories = await _dropDownListHelper.GetDDLCategoriesAsync();
+            return View(addProductViewModel);
+        }
+
     }
 }
--- ECommerce_Sat/ECommerce_Sat/Models/EditUserViewModel.cs
+++ ECommerce_Sat/ECommerce_Sat/Models/EditUserViewModel.cs
@@ -8,27 +8,27 @@ namespace ECommerce_Sat.Models
     public class EditUserViewModel : Entity
     {
         [Display(Name = "Documento")]
-        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Document { get; set; }
 
         [Display(Name = "Nombres")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string FirstName { get; set; }
 
         [Display(Name = "Apellidos")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string LastName { get; set; }
 
         [Display(Name = "Dirección")]
-        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Address { get; set; }
 
         [Display(Name = "Teléfono")]
-        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string PhoneNumber { get; set; }
 
