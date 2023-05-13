f4214c23483f88ae83bc8f683af28a69d365717f
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-13 10:37:47 -0500

    Index product action created

---
+++ ECommerce_Sat/ECommerce_Sat/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce_Sat.DAL;
+using ECommerce_Sat.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce_Sat.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly DataBaseContext _context;
+        private readonly IAzureBlobHelper _azureBlobHelper;
+        private readonly IDropDownListHelper _dropDownListHelper;
+
+        public ProductsController(DataBaseContext context, IAzureBlobHelper azureBlobHelper, IDropDownListHelper dropDownListHelper)
+        {
+            _context = context;
+            _azureBlobHelper = azureBlobHelper;
+            _dropDownListHelper = dropDownListHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .ThenInclude(pc => pc.Category)
+                .ToListAsync());
+        }
+    }
+}
