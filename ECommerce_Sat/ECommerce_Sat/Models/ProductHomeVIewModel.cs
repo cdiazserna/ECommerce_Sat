2585d083fc534eb42a44782a3a3d7be4ece5f6f8
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-20 11:12:16 -0500

    ProductHowViewModel deleted

--- ECommerce_Sat/ECommerce_Sat/Models/ProductHomeVIewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using ECommerce_Sat.DAL.Entities;
-
-namespace ECommerce_Sat.Models
-{
-    public class ProductHomeVIewModel
-    {
-        public ICollection<Product> Products { get; set; }
-    }
-}
