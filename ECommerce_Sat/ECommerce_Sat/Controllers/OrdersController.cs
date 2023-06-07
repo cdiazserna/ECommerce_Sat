b4f442fcc1426405dadbb9b22d8e6000c0d1d175
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-06-07 17:01:55 -0500

    Action DistpachOrder( ) created to Distpach an order.

--- ECommerce_Sat/ECommerce_Sat/Controllers/OrdersController.cs
+++ ECommerce_Sat/ECommerce_Sat/Controllers/OrdersController.cs
@@ -1,8 +1,10 @@
 ﻿using ECommerce_Sat.DAL;
 using ECommerce_Sat.DAL.Entities;
+using ECommerce_Sat.Enum;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Vereyon.Web;
 
 namespace ECommerce_Sat.Controllers
 {
@@ -10,10 +12,12 @@ namespace ECommerce_Sat.Controllers
     public class OrdersController : Controller
     {
         private readonly DataBaseContext _context;
+        private readonly IFlashMessage _flashMessage;
 
-        public OrdersController(DataBaseContext context)
+        public OrdersController(DataBaseContext context, IFlashMessage flashMessage)
         {
             _context = context;
+            _flashMessage = flashMessage;
         }
 
         public async Task<IActionResult> Index()
@@ -39,6 +43,29 @@ namespace ECommerce_Sat.Controllers
             if (order == null) return NotFound();
 
             return View(order);
+        }
+
+        public async Task<IActionResult> DispatchOrder(Guid? orderId)
+        {
+            if (orderId == null) return NotFound();
+
+            Order order = await _context.Orders.FindAsync(orderId);
+            if (order == null) return NotFound();
+
+            if (order.OrderStatus != OrderStatus.Nuevo)
+                _flashMessage.Danger(String.Format("Solo se pueden enviar pedidos que estén en estado '{0}'.", OrderStatus.Nuevo));
+
+            else
+            {
+                order.OrderStatus = OrderStatus.Despachado;
+                order.ModifiedDate = DateTime.Now;
+                _context.Orders.Update(order);
+                await _context.SaveChangesAsync();
+                _flashMessage.Confirmation(String.Format("El estado del pedido ha sido cambiado a '{0}'.", OrderStatus.Despachado));
+
+            }
+
+            return RedirectToAction(nameof(Details), new { orderId = order.Id });
         }
 
     }
